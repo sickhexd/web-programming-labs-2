180bb43ae05994a140dbc9f0d6b28b80013deb8b
Иванов <ivnvegor@gmail.com>
2024-09-11 16:40:35 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,11 +2,29 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 
 def start():
     return """<!doctype html> \
         <html> \
             <body>
                 <h1>web-сервер на flask</h1>
+                <a href="/author">author</a>
+            </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Антонов Семен Андреевич"
+    group = "ФБИ-21"
+    faculty = "ФБ"
+
+    return """<!doctype>
+        <html>
+            <body>
+                <p>Студент: """+ name +"""</p>
+                <p>Группа: """+ group +"""</p>
+                <p>Факультет: """+ faculty +"""</p>
+                <a href="/web">web</a>
             </body>
         </html>"""
