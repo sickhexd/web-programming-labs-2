c96d24324d5d75bd79233215a5a334111b1f2cbf
Иванов <ivnvegor@gmail.com>
2024-09-11 16:56:45 +0700

    Дуб

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,6 +10,7 @@ def start():
             <body>
                 <h1>web-сервер на flask</h1>
                 <a href="/author">author</a>
+                <a href="/lab1/oak">Oak</a>
             </body>
         </html>"""
 
@@ -28,3 +29,17 @@ def author():
                 <a href="/web">web</a>
             </body>
         </html>"""
+
+@app.route("/lab1/oak")
+def oak():
+    path = url_for("static", filename = "oak.png")
+    return '''
+    <!doctype>
+        <html>
+            <body>
+                <h1>Дуб</h1>
+                <img src="'''+ path +'''">
+                <a href="/web">web</a>
+            </body>
+        </html>
+    '''
