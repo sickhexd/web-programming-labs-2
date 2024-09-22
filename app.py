3c23462ee313dfec3cb7c3990018479316978ba8
sickhexd <sabiaflex0@gmail.com>
2024-09-22 21:07:53 +0700

    Добавление цветка

--- app.py
+++ app.py
@@ -194,10 +194,26 @@ def a2():
     return 'без слэша'
 
 
-flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
     if flower_id >= len(flower_list):
         return "Такого цветка нет", 404
     else:
         return "Цветок: " + flower_list[flower_id]
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+        <h1>Добавлен новый цветок</h1>
+        <p>Название нового цветка: {name}</p>
+        <p>Всего цветков: {len(flower_list)}</p>
+        <p>Полный список: {flower_list}</p>
+    </body>
+</html>
+'''
+
