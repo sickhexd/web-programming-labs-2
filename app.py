618d75dae3f3d2b1bfbba738315b93999454748c
sickhexd <sabiaflex0@gmail.com>
2024-09-23 20:55:38 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -223,7 +223,15 @@ def example():
     lab_num = '2'
     group = 'ФБИ-21'
     name = 'Антонов Семен'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+
+    ]
     return render_template('example.html', name = name, 
                            course = course, 
                            lab_num = lab_num, 
-                           group=group )
+                           group=group, fruits = fruits )
