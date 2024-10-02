68e9953263312c44e31742dd364130c7303881f3
sickhexd <sabiaflex0@gmail.com>
2024-10-02 20:17:56 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,6 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template , request, make_response, redirect
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3.html')
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3.html', name=name,
+                            name_color=name_color)
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    resp = make_response(redirect('/lab3'))
+    resp.set_cookie('name', 'Sam', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'red')
+    return resp
