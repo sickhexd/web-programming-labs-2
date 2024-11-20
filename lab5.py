0efc2ebce7a485894d5dedba6057038fa219172f
sickhexd <sabiaflex0@gmail.com>
2024-11-20 16:48:09 +0700

    Подключение бд SQLite

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, current_app
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
