5ff9311b7fc59abd19b1b408298fca3ed65e1ea9
sickhexd <sabiaflex0@gmail.com>
2024-11-20 16:45:10 +0700

    Подключение бд SQLite

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
-
+import os
 app = Flask(__name__)
 
-app.secret_key = 'секретынй код'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный-секрет')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
