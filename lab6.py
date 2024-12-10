21f03fa0a2313a4d350f8d6820f65af2bf6eeae8
sickhexd <sabiaflex0@gmail.com>
2024-12-11 02:04:28 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -50,6 +50,35 @@ def api():
                 'result': 'success',
                 'id': id
             }
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if not office['tenant']:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'You cannot cancel someone else\'s booking'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ''  # Освобождаем офис
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }   
+
     return {
         'jsonrpc': '2.0',
         'error': {
