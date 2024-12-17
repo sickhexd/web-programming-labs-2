e013aa78510b549f06692a5949f20b2e4dd58284
sickhexd <sabiaflex0@gmail.com>
2024-12-17 16:45:49 +0700

    Имя

--- lab9.py
+++ lab9.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import flash
 
 lab9 = Blueprint('lab9', __name__)
 
-@lab9.route('/lab9/')
+@lab9.route('/lab9/', methods=['GET', 'POST'])
 def main():
-    return render_template('lab9/index.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        if not name:
+            flash('Имя не может быть пустым.', 'error')
+        else:
+            session['name'] = name
+            return redirect(url_for('form_age'))
+    return render_template('lab9/name.html')
